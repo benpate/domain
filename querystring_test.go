9b10164f4156ee46c447741b27ae89e1f8d89286
Ben Pate <ben@pate.org>
2024-10-14 11:50:07 -0600

    Clean up test case

--- querystring_test.go
+++ querystring_test.go
@@ -4,17 +4,16 @@ import (
 	"net/url"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/require"
 )
 
 func TestQueryString(t *testing.T) {
 
-	uri, err := url.ParseQuery("http://localhost:8080/api/v1/employees?name=John&age=30&city=New%20York&city=San%20Diego&city=Los%20Angeles")
+	uri, err := url.ParseQuery("name=John&age=30&city=New%20York&city=San%20Diego&city=Los%20Angeles")
 
 	require.Nil(t, err)
+	require.Equal(t, "John", uri.Get("name"))
+	require.Equal(t, "30", uri.Get("age"))
 	require.Equal(t, "New York", uri.Get("city"))
-
 	require.Equal(t, []string{"New York", "San Diego", "Los Angeles"}, uri["city"])
-	spew.Dump(uri)
 }
