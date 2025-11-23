8d07b1798954fbb7fc7d00ecb38e4b503725a265
Ben Pate <ben@pate.org>
2025-11-22 17:05:12 -0700

    Add Host() function

---
+++ host.go
@@ -0,0 +1,18 @@
+package domain
+
+import (
+	"regexp"
+)
+
+var hostRegexp = regexp.MustCompile("^https?://[^/]+")
+
+// Host returns the protocol and hostname
+
+func Host(url string) string {
+
+	if result := hostRegexp.FindStringSubmatch(url); len(result) > 0 {
+		return result[0]
+	}
+
+	return ""
+}
---
+++ host_test.go
@@ -0,0 +1,25 @@
+package domain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHost(t *testing.T) {
+
+	do := func(input string, expected string) {
+		require.Equal(t, expected, Host(input))
+	}
+
+	do("http://localhost", "http://localhost")
+	do("http://localhost/", "http://localhost")
+	do("http://localhost:8080/some-path-name", "http://localhost:8080")
+	do("http://localhost/some-path-name", "http://localhost")
+
+	do("https://server.com", "https://server.com")
+	do("https://server.com/", "https://server.com")
+	do("https://server.com/some-path-name", "https://server.com")
+	do("https://server.com/many/path/names", "https://server.com")
+	do("https://server.com:443", "https://server.com:443")
+}
